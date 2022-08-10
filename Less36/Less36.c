375a9a52bd6b227ba4289def9684142e2f6d7c40
Vlad Krutyakov <smehnoi32@gmail.com>
2022-08-10 16:18:30 +0400

    not the final version

--- Less36/Less36.c
+++ Less36/Less36.c
@@ -1,7 +1,7 @@
 /*1) Запишите в файл 10 любых строк текста. Проверьте правильность записи с помощью любого редактора.
 2) Прочитайте все строки из файла из задания 1 и отобразите на экране.
 Измените файл с помощью любого редактора, добавив или удалив строки, и снова прочитайте его с помощью программы.
-3) С помощью любого редактора создайте файл и запишите в него несколько чисел.
+3) С помощью любого редактора создайте файл и запишите в него 5 чисел.
 Считайте все числа из файла удвойте их значение и отобразите на экране.
 */
 #include <stdio.h>
@@ -27,21 +27,7 @@ void filingArr(){
     }
 }
 
-int main(){
-
-    FILE *f;
-    int d;
-    float a;
-
-    /*filingArr();
-
-    f = fopen("001.txt", "w");
-        for (int i = 0; i < MasLenght; i++) {
-            fprintf(f, "%s\n", arr1[i]);
-        }
-    fclose(f);
-    */
-
+void displaying(FILE *f){
     f = fopen("001.txt", "r");
     if (f == NULL)
         printf("fopen read error");
@@ -50,14 +36,75 @@ int main(){
             fscanf(f, "%s", arr1[i]);
             printf("%s\n", arr1[i]);
         }
-        /*
-        fscanf(f, "%d", &d);
-        fscanf(f, "%g", &a);
-        printf("%d   %g\n", d, a);
-        */
     }
     fclose(f);
+}
 
+void firstPoint(FILE *f){
+    filingArr();
+    f = fopen("001.txt", "w");
+        for (int i = 0; i < MasLenght; i++) {
+            fprintf(f, "%s\n", arr1[i]);
+        }
+    fclose(f);
+
+    printf("The '001.txt' file contains 10 lines of text. Check the correctness of the entry in any text editor and go further.\n");
     system("pause");
+}
+
+void secondPoint(FILE *f){
+    system("cls");
+    displaying(f);
+
+    printf("The contents of the '001.txt' file are displayed on the screen.\nOpen this file in any text editor and change its contents. Then press any key.\n");
+    system("pause");
+
+    displaying(f);
+    printf("Compare the values\n");
+    system("pause");
+}
+
+void thirdPoint(FILE *f){
+    char name[20];
+    float a;
+    printf("Use any editor to create a text file and write some numbers into it.\nWrite down each number from a new line.\nEnter the name of the text file in the 'name.txt' format:");
+    scanf("%s", name);
+    f = fopen(name, "r");
+    if (f == NULL)
+        printf("fopen read error");
+    else {
+        for (int i = 0; i < 5; i++) {      
+            fscanf(f, "%g", &a);
+            printf("%g\n", a*2);
+        }
+    }
+    fclose(f);
+    system("pause");
+}
+
+int main(){
+
+    FILE *f;
+    int tn;
+
+    printf("Enter numper task:");
+    scanf("%d", &tn);
+
+    switch (tn)
+    {
+    case 1:
+        firstPoint(f);
+        break;
+    case 2:
+        secondPoint(f);
+        break;
+    case 3:
+        thirdPoint(f);
+        break;
+    
+    default: printf("incorrect number");
+        break;
+    }
+
     return 0;
 }
