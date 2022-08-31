94c250db17b33aa39798aad3f27cb3967d9c8a47
Vlad Krutyakov <smehnoi32@gmail.com>
2022-08-31 16:04:25 +0400

    add map00

--- Less39/quest.c
+++ Less39/quest.c
@@ -10,46 +10,64 @@
 #define width 80
 #define height 25
 
-struct {
+struct {                            //Структура данных о локации
     char map[height][width+1];
 } loc;
 
-void loc_LoadFromFile(char *fileName) {
-    memset(&loc.map, ' ', sizeof(loc));
-    for (int i = 0; i < height; i++) {
+void loc_LoadFromFile(char *fileName) {     //Процедура загрузки в локацию информации из файла
+    memset(&loc.map, ' ', sizeof(loc));     //Заполнение матрицы map символом пробела
+    for (int i = 0; i < height; i++) {      //Заполнение последнего столбца символом конца строки
         loc.map[i][width] = '\0';
     }
 
-    FILE *f = fopen(fileName, "r");
-    char c[80];
-    int line = 0;
-    while(!feof(f)){
+    FILE *f = fopen(fileName, "r");     //Открытие файла на чтение
+    char c[80];     //переменная хранящая текущую строку заполенеия локации
+    int line = 0;   //Заполнение начинается с нулевой строки
+    while(!feof(f)){        //Читаем кажду строку, пока файл не кончится
         fgets(c, width, f);
-        int cnt = strlen(c);
-        if (c[cnt-1] == '\n') cnt--;
-        strcpy(loc.map[line], c, cnt);
-        line++;
+        int cnt = strlen(c);        //длина строки без символа конца строки
+        if (c[cnt-1] == '\n') cnt--;        //Если в конце строки стоит символ перевода строки, то уменьшаем длину на 1
+        strncpy(loc.map[line], c, cnt);      //Копируем все в текущую строку локации
+        line++;                             //Переход на следущую строку
     }
     fclose(f);
 
-    loc.map[height-1][width-1] = '\0';
+    loc.map[height-1][width-1] = '\0';      //Последний символ последний строки заполняем символом конца строки, чтобы курсор не переходил на следуюшую строку
 }
 
-char map[height][width+1];
+char map[height][width+1];      //массив хранящий карту
 
-void map_Show() {
+void map_Show() {       //Процедура отображения карты на экране
     for (int i = 0; i < height; i++) {
         printf("%s\n", loc.map[i]);
     }
 }
 
-void loc_PutOnMap () {
-    memcpy(map, loc.map, sizeof(map));
+void loc_PutOnMap () {      //Процедура копирования локации перед отображением
+    memcpy(map, loc.map, sizeof(map));      //Копирование всех байтов из источника (loc.map) в приёмник (map)
+}
+
+struct {
+    POINT pos;
+} player;
+
+void player_Init (int x, int y) {
+    player.pos.x = x;
+    player.pos.y = y;
+}
+
+void player_PutOnMap(){
+    map[player.pos.y][player.pos.x] = 'A';
 }
 
 int main () {
+
+    player_Init(5,5);
     loc_LoadFromFile("map_0_0.txt");
+    loc_PutOnMap();
+    player_PutOnMap();
     map_Show();
 
+    system("pause");
     return 0;
 }
