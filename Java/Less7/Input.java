8fba927cff4d3c17b9ffa1b85c86d05d004670f6
Vlad Krut <smehnoi32@gmail.com>
2022-09-21 16:51:02 +0400

    Less7 complited withot encoding

---
+++ Java/Less7/Input.java
@@ -0,0 +1,12 @@
+package Less7;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String str = s.nextLine();
+        System.out.println("Вы ввели: "+str);
+    }
+}
